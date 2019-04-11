4786768b9a3e66e93aa1fe829595935b3eca3bad
Andrew Bray <andrew.bray@gmail.com>
2019-04-10 17:03:33 -0700

    fix shadows

--- maps/possum-trot.R
+++ maps/possum-trot.R
@@ -10,12 +10,11 @@ mat <- elev_matrix(lon = c(-89.586, -89.603),
                    zoom = 14)
 
 my_z <- 2
-ambmat <- ambient_shade(mat)
 
 mat %>%
   sphere_shade(texture = "desert")  %>%
-  add_shadow(ray_shade(mat, zscale=3, maxsearch = 300), 0.5) %>%
-  add_shadow(ambmat,0.5) %>%
+  add_shadow(ray_shade(mat, zscale=my_z), 0.5) %>%
+  add_shadow(ambient_shade(mat), 0.5) %>%
   plot_3d(mat, zscale = my_z)
 
-save_3dprint("possum-trot.stl", maxwidth = 200, unit = "mm")
+#save_3dprint("possum-trot.stl", maxwidth = 200, unit = "mm")
